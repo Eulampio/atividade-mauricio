f89dd6f49fab21ed6038775ce5e59cdeefddb755
Eulampio <alesantoseulampio@gmail.com>
2024-11-08 18:36:32 -0300

    xxx
    xxxxx

---
+++ at01.py
@@ -0,0 +1,8 @@
+def soma (a,b):
+    x=a+b
+    return x
+while True:
+    a=float(input("digite o valor"))
+    b=float(input("digite o valor"))
+    resu= soma(2,2)
+    print(resu)
---
+++ at02.py
@@ -0,0 +1,7 @@
+def maior(x,y):
+    if x>y:
+        return True
+    elif y>x:
+        return False
+while True:
+    
